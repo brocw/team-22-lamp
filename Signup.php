2c350711ae8b3902bf6119f1c118e94137a0aa50
Ana Clara Machado Goncalves <anaaclra04@gmail.com>
2026-01-30 13:58:45 -0500

    Add AddContact.php and Signup.php

---
+++ Signup.php
@@ -0,0 +1,57 @@
+<?php
+
+	$inData = getRequestInfo();
+	
+	$id = 0;
+	$firstName = "";
+	$lastName = "";
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331"); 	
+	if( $conn->connect_error )
+	{
+		returnWithError( $conn->connect_error );
+	}
+	else
+	{
+		$stmt = $conn->prepare("INSERT INTO Users (Login, Password, firstName, lastName) VALUES(?, ?, ?, ?)");
+		$stmt->bind_param("ssss", $inData["login"], $inData["password"], $inData["firstName"], $inData["lastName"]);
+		$stmt->execute();
+		
+		if( $stmt->affected_rows > 0 )
+		{
+			$id = $conn->insert_id;
+			returnWithInfo( $inData["firstName"], $inData["lastName"], $id );
+		}
+		else
+		{
+			returnWithError("Registration failed");
+		}
+
+		$stmt->close();
+		$conn->close();
+	}
+	
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+	function returnWithInfo( $firstName, $lastName, $id )
+	{
+		$retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
