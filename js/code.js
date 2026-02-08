c1437ba5547302ab4708d5470860d33fb1edaf71
LGober <la236357@ucf.edu>
2026-02-08 00:22:41 -0500

    added contact adding, registration, most of deletion, and general ui changes

--- js/code.js
+++ js/code.js
@@ -1,4 +1,5 @@
-const urlBase = 'http://cop-4331-22.com/LAMPAPI';
+// LOCAL_ONLY_TOGGLE: revert to production when told
+const urlBase = 'http://localhost:8000/LAMPAPI';
 const extension = 'php';
 
 let userId = 0;
@@ -58,6 +59,47 @@ function doLogin()
 
 }
 
+function createAccount()
+{
+	let firstName = document.getElementById("newAccountFirstName").value;
+	let lastName = document.getElementById("newAccountLastName").value;
+	let login = document.getElementById("newAccountUsername").value;
+	let password = document.getElementById("newAccountPassword").value;
+	var hash = md5( password );
+	var tmp = {firstName: firstName, lastName: lastName, login: login, password: hash};
+	let jsonPayload = JSON.stringify( tmp );
+	
+	let url = urlBase + '/Signup.' + extension;
+
+	document.getElementById("loginResult").innerHTML = "";
+
+	let xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		xhr.onreadystatechange = function()
+		{
+			if (this.readyState == 4 && this.status == 200)
+			{
+				let jsonObject = JSON.parse( xhr.responseText );
+				if( jsonObject.error && jsonObject.error.length > 0 )
+				{
+					document.getElementById("loginResult").innerHTML = jsonObject.error;
+					return;
+				}
+				document.getElementById("loginResult").innerHTML = "Account created. Redirecting...";
+				setTimeout(function() { window.location.href = "login.html"; }, 800);
+			}
+		};
+		xhr.send(jsonPayload);
+	}
+	catch(err)
+	{
+		document.getElementById("loginResult").innerHTML = err.message;
+	}
+}
+
 function saveCookie()
 {
 	let minutes = 20;
@@ -70,6 +112,10 @@ function readCookie()
 {
 	userId = -1;
 	let data = document.cookie;
+	// LOCAL_PREVIEW_BYPASS_START (revert for production)
+	let host = window.location.hostname;
+	let isLocal = window.location.protocol === "file:" || host === "localhost" || host === "127.0.0.1" || host === "0.0.0.0" || host === "::1";
+	// LOCAL_PREVIEW_BYPASS_END (revert for production)
 	let splits = data.split(",");
 	for(var i = 0; i < splits.length; i++) 
 	{
@@ -91,7 +137,18 @@ function readCookie()
 	
 	if( userId < 0 )
 	{
-		window.location.href = "index.html";
+		// LOCAL_PREVIEW_BYPASS_START (revert for production)
+		if( isLocal )
+		{
+			userId = 1;
+			firstName = "Local";
+			lastName = "Preview";
+		}
+		else
+		{
+			window.location.href = "index.html";
+		}
+		// LOCAL_PREVIEW_BYPASS_END (revert for production)
 	}
 	else
 	{
@@ -108,15 +165,17 @@ function doLogout()
 	window.location.href = "index.html";
 }
 
-function addColor()
+function addContact()
 {
-	let newColor = document.getElementById("colorText").value;
-	document.getElementById("colorAddResult").innerHTML = "";
+	let firstName = document.getElementById("addContactFirstName").value;
+	let lastName = document.getElementById("addContactLastName").value;
+	let phoneNumber = document.getElementById("addContactPhone").value;
+	let email = document.getElementById("addContactEmail").value;
 
-	let tmp = {color:newColor,userId,userId};
+	let tmp = {firstName:firstName, lastName:lastName, email:email, phoneNumber:phoneNumber,userId:userId};
 	let jsonPayload = JSON.stringify( tmp );
 
-	let url = urlBase + '/AddColor.' + extension;
+	let url = urlBase + '/AddContact.' + extension;
 	
 	let xhr = new XMLHttpRequest();
 	xhr.open("POST", url, true);
@@ -127,29 +186,24 @@ function addColor()
 		{
 			if (this.readyState == 4 && this.status == 200) 
 			{
-				document.getElementById("colorAddResult").innerHTML = "Color has been added";
+				document.getElementById("contactAddResult").innerHTML = "Contact has been added";
 			}
 		};
 		xhr.send(jsonPayload);
 	}
 	catch(err)
 	{
-		document.getElementById("colorAddResult").innerHTML = err.message;
+		document.getElementById("contactAddResult").innerHTML = err.message;
 	}
 	
 }
 
-function searchColor()
+function deleteContact(contactId)
 {
-	let srch = document.getElementById("searchText").value;
-	document.getElementById("colorSearchResult").innerHTML = "";
-	
-	let colorList = "";
-
-	let tmp = {search:srch,userId:userId};
+	let tmp = {userId:userId, contactId:contactId};
 	let jsonPayload = JSON.stringify( tmp );
 
-	let url = urlBase + '/SearchColors.' + extension;
+	let url = urlBase + '/DeleteContact.' + extension;
 	
 	let xhr = new XMLHttpRequest();
 	xhr.open("POST", url, true);
@@ -160,26 +214,74 @@ function searchColor()
 		{
 			if (this.readyState == 4 && this.status == 200) 
 			{
-				document.getElementById("colorSearchResult").innerHTML = "Color(s) has been retrieved";
-				let jsonObject = JSON.parse( xhr.responseText );
-				
-				for( let i=0; i<jsonObject.results.length; i++ )
-				{
-					colorList += jsonObject.results[i];
-					if( i < jsonObject.results.length - 1 )
-					{
-						colorList += "<br />\r\n";
-					}
-				}
-				
-				document.getElementsByTagName("p")[0].innerHTML = colorList;
+				document.getElementById("contactDeleteResult").innerHTML = "Contact has been deleted";
+				searchContact();
 			}
 		};
 		xhr.send(jsonPayload);
 	}
 	catch(err)
 	{
-		document.getElementById("colorSearchResult").innerHTML = err.message;
+		document.getElementById("contactDeleteResult").innerHTML = err.message;
+	}
+}
+
+function toggleAddContactDiv(){
+	let addContactDiv = document.getElementById("addContactDiv");
+	if (addContactDiv.style.display === "none" || addContactDiv.style.display === "") {
+		addContactDiv.style.display = "flex";
+	} else {
+		addContactDiv.style.display = "none";
+	}
+}
+
+function searchContact()
+{
+	let srch = document.getElementById("searchText").value;
+	document.getElementById("contactSearchResult").innerHTML = "";
+	
+	let contactList = "";
+
+	let tmp = {search:srch,userId:userId};
+	let jsonPayload = JSON.stringify( tmp );
+
+	let url = urlBase + '/SearchContact.' + extension;
+	
+	let xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		xhr.onreadystatechange = function() 
+		{
+			if (this.readyState == 4 && this.status == 200) 
+			{
+				document.getElementById("contactSearchResult").innerHTML = "Contact(s) has been retrieved";
+				let jsonObject = JSON.parse( xhr.responseText );
+				
+				for( let i=0; i<jsonObject.results.length; i++ )
+				{
+					let contact = jsonObject.results[i];
+					contactList += contact.firstName + " " + contact.lastName +
+						" | " + contact.phone + " | " + contact.email +
+						" <button type=\"button\" class=\"deleteContactButton\" data-contact-id=\"" + contact.id + "\" onclick=\"deleteContact(" + contact.id + ");\">" +
+						"<img src=\"images/managerButtons/oceanXButton.png\" alt=\"Delete Contact\" /></button>" +
+						" <button type=\"button\" class=\"editContactButton\" data-contact-id=\"" + contact.id + "\" onclick=\"editContact(" + contact.id + ");\">" + /*!!!!IMPORTANT!!!! needs to be replaced with a function that will use add and delete to effectivly edit a contact*/
+						"<img src=\"images/managerButtons/oceanEditButton.png\" alt=\"Edit Contact\" /></button>";
+					if( i < jsonObject.results.length - 1 )
+					{
+						contactList += "<br />\r\n";
+					}
+				}
+				
+				document.getElementById("contactList").innerHTML = contactList;
+			}
+		};
+		xhr.send(jsonPayload);
+	}
+	catch(err)
+	{
+		document.getElementById("contactSearchResult").innerHTML = err.message;
 	}
 	
 }
