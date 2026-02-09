03901b16e29c49927c7731a42769d1578f658be9
Ana Clara Machado Goncalves <amachadogoncalves@mail.valenciacollege.edu>
2026-02-09 16:50:41 -0500

    Fix Signup.php: add duplicate user check

--- LAMPAPI/Signup.php
+++ LAMPAPI/Signup.php
@@ -1,57 +1,65 @@
 <?php
 
-	$inData = getRequestInfo();
-	
-	$id = 0;
-	$firstName = "";
-	$lastName = "";
+    $inData = getRequestInfo();
 
-	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331"); 	
-	if( $conn->connect_error )
-	{
-		returnWithError( $conn->connect_error );
-	}
-	else
-	{
-		$stmt = $conn->prepare("INSERT INTO Users (Login, Password, firstName, lastName) VALUES(?, ?, ?, ?)");
-		$stmt->bind_param("ssss", $inData["login"], $inData["password"], $inData["firstName"], $inData["lastName"]);
-		$stmt->execute();
-		
-		if( $stmt->affected_rows > 0 )
-		{
-			$id = $conn->insert_id;
-			returnWithInfo( $inData["firstName"], $inData["lastName"], $id );
-		}
-		else
-		{
-			returnWithError("Registration failed");
-		}
+    $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+    if ($conn->connect_error) 
+    {
+        returnWithError( $conn->connect_error );
+    } 
+    else
+    {
+        // Check if Login already exists
+        $stmt = $conn->prepare("SELECT ID FROM Users WHERE Login=?");
+        $stmt->bind_param("s", $inData["login"]);
+        $stmt->execute();
+        $result = $stmt->get_result();
 
-		$stmt->close();
-		$conn->close();
-	}
-	
-	function getRequestInfo()
-	{
-		return json_decode(file_get_contents('php://input'), true);
-	}
+        if ($result->num_rows > 0)
+        {
+            $stmt->close();
+            $conn->close();
+            returnWithError("Username already exists");
+        }
+        else
+        {
+            $stmt->close();
 
-	function sendResultInfoAsJson( $obj )
-	{
-		header('Content-type: application/json');
-		echo $obj;
-	}
-	
-	function returnWithError( $err )
-	{
-		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
-	}
-	
-	function returnWithInfo( $firstName, $lastName, $id )
-	{
-		$retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
-		sendResultInfoAsJson( $retValue );
-	}
-	
+            $stmt = $conn->prepare("INSERT into Users (FirstName, LastName, Login, Password) VALUES(?,?,?,?)");
+            $hashedPassword = md5($inData["password"]);
+            $stmt->bind_param("ssss", $inData["firstName"], $inData["lastName"], $inData["login"], $hashedPassword);
+            $stmt->execute();
+
+            $id = $stmt->insert_id;
+
+            $stmt->close();
+            $conn->close();
+
+            returnWithInfo($inData["firstName"], $inData["lastName"], $id);
+        }
+    }
+
+    function getRequestInfo()
+    {
+        return json_decode(file_get_contents('php://input'), true);
+    }
+
+    function sendResultInfoAsJson( $obj )
+    {
+        header('Content-type: application/json');
+        echo $obj;
+    }
+
+    function returnWithError( $err )
+    {
+        $retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+        sendResultInfoAsJson( $retValue );
+    }
+
+    function returnWithInfo( $firstName, $lastName, $id )
+    {
+        $retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
+        sendResultInfoAsJson( $retValue );
+    }
+
 ?>
