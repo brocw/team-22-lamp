b06338fc2421c0ec63ec3c4668fa10f01d6ce9c3
Ana Clara Machado Goncalves <amachadogoncalves@mail.valenciacollege.edu>
2026-02-11 13:34:44 -0500

    update AddContact.php endpoint, fix syntax

--- LAMPAPI/AddContact.php
+++ LAMPAPI/AddContact.php
@@ -1,57 +1,50 @@
 <?php
-    #{
-        #"firstName": "John",
-        #"lastName": "Doe",
-        #"email": "john@example.com",
-        #"phoneNumber": "000-555-1234",
-        #"userId": "1"
-    #}
-    #error_reporting(E_ALL);
-    #ini_set('display_errors', 1);
+        error_reporting(E_ALL);
+        ini_set('display_errors', 1);
 
-    $inData = getRequestInfo();
+        $inData = getRequestInfo();
 
-    $firstName = $inData["firstName"];
-    $lastName = $inData["lastName"];
-    $email = $inData["email"];
-    $phoneNumber = $inData["phoneNumber"];
-    $userId = $inData["userId"];
+        $firstName = $inData["firstName"];
+        $lastName = $inData["lastName"];
+        $email = $inData["email"];
+        $phoneNumber = $inData["phoneNumber"];
+        $userId = $inData["userId"];
 
-    $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
-    if ($conn->connect_error) 
-    {
-        returnWithError( $conn->connect_error );
-    } 
-    else
-    {
-        $stmt = $conn->prepare("INSERT into Contacts (FirstName, LastName, Email, PhoneNumber, ID) VALUES(?, ?, ?, ?, ?)");
-        $stmt->bind_param("ssssi", $firstName, $lastName, $email, $phoneNumber, $userId);
-        $stmt->execute();
-        $stmt->close();
-        $conn->close();
-        returnWithInfo( "Contact has been added" );
-    }
+        $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+        if ($conn->connect_error) 
+        {
+                returnWithError( $conn->connect_error );
+        } 
+        else
+        {
+                $stmt = $conn->prepare("INSERT into Contacts (FirstName, LastName, Email, PhoneNumber,UserID) VALUES(?, ?, ?, ?, ?)");
+                $stmt->bind_param("ssssi", $firstName, $lastName, $email, $phoneNumber, $userId);
+                $stmt->execute();
+                $stmt->close();
+                $conn->close();
+                returnWithInfo( "Contact has been added" );
+        }
 
-    function getRequestInfo()
-    {
-        return json_decode(file_get_contents('php://input'), true);
-    }
+        function getRequestInfo()
+        {
+                return json_decode(file_get_contents('php://input'), true);
+        }
 
-    function sendResultInfoAsJson( $obj )
-    {
-    	header('Content-type: application/json');
-        echo $obj;
-    }
+        function sendResultInfoAsJson( $obj )
+        {
+                header('Content-type: application/json');
+                echo $obj;
+        }
 
-    function returnWithError( $err )
-    {
-    	$retValue = '{"error":"' . $err . '"}';
-        sendResultInfoAsJson( $retValue );
-    }
+        function returnWithError( $err )
+        {
+                $retValue = '{"error":"' . $err . '"}';
+                sendResultInfoAsJson( $retValue );
+        }
 
-    function returnWithInfo( $msg )
-    {
-        $retValue = '{"message":"' . $msg . '","error":""}';
-        sendResultInfoAsJson( $retValue );
-    }
+        function returnWithInfo( $msg )
+        {
+                $retValue = '{"message":"' . $msg . '","error":""}';
+                sendResultInfoAsJson( $retValue );
+        }
 ?>
